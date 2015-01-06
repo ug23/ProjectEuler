e17e70c8110395d680c3178b575b5b73af1622c4
Yuji Imagawa <using923@gmail.com>
2015-01-06 19:51:14 +0900

    done prob. 002

---
+++ Scala/002.scala
@@ -0,0 +1,7 @@
+def fibList(f0: Int, f1: Int, lim: Int): List[Int] = {
+  val f2 = f0 + f1
+  if (f2 > lim) Nil else f2 :: fibList(f1, f2, lim)
+}
+def fibList(lim: Int): List[Int] = 1 :: 2 :: fibList(1, 2, lim)
+val LIMIT = 4000000
+println(fibList(LIMIT).filter(i => i % 2 == 0).sum)

1da702c165ac6127cf0d3d09982b7af34e269c82
Yuji Imagawa <using923@gmail.com>
2015-01-06 21:39:44 +0900

    done prob. 006

---
+++ Scala/006.scala
@@ -0,0 +1,12 @@
+// first one hundred natural numbers
+val fohnn = Range(1, 100 + 1)
+
+// the sum of the squares
+val totalSquares = fohnn.map(x => x * x).sum
+println("the sum of the squares: " + totalSquares)
+
+// the square of the sum
+val totals = fohnn.sum * fohnn.sum
+println("the square of the sum: " + totals)
+
+println("Ans. "+Math.abs(totalSquares - totals))

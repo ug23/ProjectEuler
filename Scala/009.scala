4a43553eed4d45876d3af2b98e5a6f918dc5d321
Yuji Imagawa <using923@gmail.com>
2015-01-18 22:24:12 +0900

    009 done

--- Scala/009.scala
+++ Scala/009.scala
@@ -2,24 +2,23 @@
 // There exists exactly one Pythagorean triplet
 // for which a + b + c = 1000. Find the product abc.
 
+
 import scala.math._
 
-// 005より
-def gcd(a: Int, b: Int): Int ={
-  if (b == 0) a
-  else gcd(b, a % b)
-}
+def sq(x: Long) = x * x
 
-def sqrtToInt(n:Int):Int = ceil(sqrt(n)).toInt
+// 差が奇数になる[1,1000]の範囲の自然数の組を用意
+val pairs = Range(1, 1000+1).combinations(2).toSeq
+  .filter(p => (p(1) - p(0)) % 2 == 1 )
 
-def isCoprime(p:Seq[Int]):Boolean = gcd(p(0),p(1)) == 1
+// 上の自然数の組から三つ組をつくる
+// (m,n) => (m^2 - n^2, 2mn, m^2+n^2)
+val triplets = pairs.map(p => 
+  Seq(sq(p(1).toLong) - sq(p(0).toLong),
+   2 * p(0).toLong * p(1).toLong,
+    sq(p(1).toLong) + sq(p(0).toLong)))
+    .filter(s => s.sum == 1000)
 
-
-
-// a+b+c == 1000 , a=m^2-n^2, b=2mn, c=m^2+n^2
-// a+b+c = (m^2-n^2)/2 + mn + (m^2 + n^2)/2
-//       = m^2 +mn = 1000
-
-val pair = Range(1,1000,2).toSeq.combinations(2)
-val coprimer = pair.filter(p=>isCoprime(p)).
-coprimer.foreach(println)
+val ans = triplets.head
+println("(a,b,c) = ("+ans.mkString(",")+")")
+println("Ans: abc = "+ans.product)

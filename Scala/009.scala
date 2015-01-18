08e6fde1ea5ba5828f22b5b168996cdd1e3a8a14
Yuji Imagawa <using923@gmail.com>
2015-01-18 22:28:05 +0900

    フォーマット直し 009

--- Scala/009.scala
+++ Scala/009.scala
@@ -2,23 +2,22 @@
 // There exists exactly one Pythagorean triplet
 // for which a + b + c = 1000. Find the product abc.
 
-
 import scala.math._
 
 def sq(x: Long) = x * x
 
 // 差が奇数になる[1,1000]の範囲の自然数の組を用意
-val pairs = Range(1, 1000+1).combinations(2).toSeq
-  .filter(p => (p(1) - p(0)) % 2 == 1 )
+val pairs = Range(1, 1000 + 1).combinations(2).toSeq
+  .filter(p => (p(1) - p(0)) % 2 == 1)
 
 // 上の自然数の組から三つ組をつくる
 // (m,n) => (m^2 - n^2, 2mn, m^2+n^2)
-val triplets = pairs.map(p => 
+val triplets = pairs.map(p =>
   Seq(sq(p(1).toLong) - sq(p(0).toLong),
-   2 * p(0).toLong * p(1).toLong,
+    2 * p(0).toLong * p(1).toLong,
     sq(p(1).toLong) + sq(p(0).toLong)))
-    .filter(s => s.sum == 1000)
+  .filter(s => s.sum == 1000)
 
 val ans = triplets.head
-println("(a,b,c) = ("+ans.mkString(",")+")")
-println("Ans: abc = "+ans.product)
+println("(a,b,c) = (" + ans.mkString(",") + ")")
+println("Ans: abc = " + ans.product)

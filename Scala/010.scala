9fcbac733d69c02ec0057dec215a98ee0d25654a
Yuji Imagawa <using923@gmail.com>
2015-01-25 23:18:50 +0900

    フォーマット直し

--- Scala/010.scala
+++ Scala/010.scala
@@ -4,10 +4,10 @@
 import scala.math._
 
 // 007より無限素数リスト
-lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i => 
+lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(i =>
   ps.takeWhile(j => j * j <= i).forall(i % _ > 0))
 
 // 有限にしてLongにしてあげる
 val underTM = ps.takeWhile(_ <= 2000000).toSeq.map(_.toLong)
 
-println("Ans : "+underTM.sum)
+println("Ans : " + underTM.sum)
